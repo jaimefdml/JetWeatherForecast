eebd512652054ba4e5a629dbf93e4e39e01313c2
Jaime Fernandez de Molina Lovera <jaimefdml@gmail.com>
2024-01-09 17:57:09 +0100

    Add initial SplashScreen and prepare to Add Icon

--- app/src/main/java/com/jaimefdml/android/jetweatherforecast/screens/WeatherSplashScreen.kt
+++ app/src/main/java/com/jaimefdml/android/jetweatherforecast/screens/WeatherSplashScreen.kt
@@ -1,11 +1,36 @@
 package com.jaimefdml.android.jetweatherforecast.screens
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import java.util.NavigableMap
 
 @Composable
 fun WeatherSplashScreen(navigableMap: NavController) {
-    Text(text = "Splash Screen")
+    Surface(modifier = Modifier
+        .padding(15.dp)
+        .size(330.dp),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+    ) {
+        Column(modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+
+            Text(text = "Find the Sun")
+        }
+    }
 }
